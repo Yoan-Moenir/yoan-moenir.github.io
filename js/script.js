abbe3a2974d5cc480222fdc45a2eaa2a8061b024
yoan-moenir <yoannamunir@gmail.com>
2025-08-14 18:00:56 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+  });
+});
