e16b68debc6f4c2cfc45bb5e99af9cf478bf96ca
Yoan-Moenir <yoannamunir@gmail.com>
2025-08-16 16:01:38 +0700

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menuToggle");
-  const mobileMenu = document.getElementById("mobileMenu");
-
-  menuToggle.addEventListener("click", () => {
-    mobileMenu.classList.toggle("show");
-  });
-});
